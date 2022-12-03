17e3f25060637b723405d77e82e94fa05748bf6c
Marvelous Dev <marvelous dev.0@gmail.com>
2022-12-03 09:33:17 -0500

    add head title and meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import PageHeader from "../src/components/PageHeader";
 import HomeIcon from "../public/assets/icons/home.svg";
 import { stringOnly } from "../src/constants/strings";
 import Header from "../src/components/Header";
+import Head from "next/head";
 
 export const token =
   "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImsycXJqdEN6S2RwRnlTSnJRQ1Q0dUEiLCJ0eXAiOiJhdCtqd3QifQ.eyJuYmYiOjE2NzAwNzQzNDAsImV4cCI6MTY3MDA3Nzk0MCwiaXNzIjoiaHR0cHM6Ly9pZHAucmV2ZWFsLWRldi5uZXQiLCJhdWQiOlsiYXBpLmFwaTEiLCJhcGkudXNlcm1hbmFnZW1lbnQiXSwiY2xpZW50X2lkIjoiTVRNLXVpLXJjcC1jbGllbnQiLCJzdWIiOiI4YjNmMGRjMS0xMTEwLTQzZWQtYjE2NC1hYTEzNWRiMWUwNGEiLCJhdXRoX3RpbWUiOjE2NzAwNzQzMzUsImlkcCI6ImxvY2FsIiwiUGVyc29uSWQiOiIzMzI3OTEyIiwicGVybWlzc2lvbiI6WyJhcGkucmNwLm1hbmFnZXIiLCJhcGkucGVyc29ub3JnLnBlcnNvbiIsImFwaS51c2VybWFuYWdlbWVudC5wZXJzb24iLCJyb2xlOkFEQU1hbmFnZXIiLCJyb2xlOlBlcnNvbiJdLCJlbWFpbCI6IkJhcnJ5X1JUQ0BZb3BtYWlsLmNvbSIsInNjb3BlIjpbImlkLmN1c3RvbSIsInByb2ZpbGUiLCJvcGVuaWQiLCJhcGkuYXBpMSIsImFwaS51c2VybWFuYWdlbWVudCJdLCJhbXIiOlsicHdkIl19.S1T9BLE9Kzp_DoQMVQoN0PrqW7Wj2nMhfF7aDgwCwlX-z7OfBGswgEyN-OMsDtxDuqXP4s-BhXAtpqqYxQjuZmi4_G3mVyDKa5AZVD5VZKOBiSf8Nna5CxJQiBg_J65KCFfKuBii8K2rfvARKbZDivVFWQEOyAxzrXqHQQDIsSk8DXqN4N3W8etH6H2WPPxrdLUdT7T-d5NqDRvjkRjn-iKL8QpRl8dNyMN4HE7ns7aeFsBnaWz69C9ZRdiXG3kqW-dSVIY0oNa2xTNoHoSNDydevzNuOPj-IB1nKdTwa8lA90VZYull6It0xugJTT4EigPNMo4ZJiAR9TS46udK1g";
@@ -29,6 +30,10 @@ const Home: NextPage<{
 
   return (
     <Box>
+      <Head>
+        <title>RCP NextJs</title>
+        <meta name="description" content="Home page of RCP NextJs" />
+      </Head>
       <Header />
       <PageHeader title={strings.title}>
         <HomeIcon />
